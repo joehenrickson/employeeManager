2f04d8e07ac001aa5431037916d96ac773c38e0b
joehenrickson <joehenrickson@gmail.com>
2022-08-28 15:33:53 -0500

    added cases

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection(
-  'mysql://root:rootroot@localhost:3306/employeeManager_db'
-);
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Barnabas1005!',
+  database: 'employeeManager_db',
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
 
 const inquirer = require('inquirer');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 inquirer
   .prompt([
@@ -20,9 +28,38 @@ inquirer
         'View Employees',
         'Update Employee Role',
       ],
-      name: 'init',
+      name: 'choice',
     },
   ])
   .then((init) => {
     console.log(init);
+    console.log(init.choice);
+    switch (init.choice) {
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'Add Employee':
+        addEmployee();
+        break;
+      case 'View Departments':
+        viewDepartments();
+        break;
+      case 'View Roles':
+        viewRoles();
+        break;
+      case 'View Employees':
+        viewEmployees();
+        break;
+      case 'Update Employee Role':
+        updateRole();
+        break;
+    }
   });
+
+const addDepartment = () => {
+  console.log('you tried to add a department');
+};
+module.exports = connection;
